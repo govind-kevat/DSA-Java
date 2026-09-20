25fbdc9d2556611e977c036731f94fadf32dc5a6
Govind kevat <govindkevat958@gmail.com>
2026-09-20 18:32:28 +0530

    adding pop operation

--- Stack-Queue/src/Stack/CustomStack.java
+++ Stack-Queue/src/Stack/CustomStack.java
@@ -42,7 +42,7 @@ public static void main(String[] args) {
 
     stack.display();
 
-//    System.out.println(stack.pop());
-//    System.out.println(stack.pop());
+    System.out.println(stack.pop());
+   System.out.println(stack.pop());
 }
 }
